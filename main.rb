c9f0ecf5d32825b1fc91a4f68fe87167a5149780
Sho Hashimoto <hashimoto@shokai.org>
2014-04-24 16:56:20 +0900

    first commit

--- main.rb
+++ main.rb
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+last_access_path = nil
+
+
 get '/' do
+  @title = "たびっと"
+  puts last_access_path
   haml :index
+end
+
+
+get '/video/:id' do
+  @id = params[:id]
+  last_access_path = params[:id]
+  haml :video
+end
+
+get '/:name' do 
+  @name  = params[:name]
+  haml :memo
 end
